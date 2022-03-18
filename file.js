143e5b0e71533c78be569416a7cd3c638a469285
nervelessbuster <teslim38@gmail.com>
2022-03-18 22:15:02 +0000

    First-commit

---
+++ file.js
@@ -0,0 +1,5 @@
+const header = document.getElementById('myHeader');
+header.style.backgroundColor = 'yellow';
+// const article = document.querySelectorAll('.my-article');
+const article1 = document.getElementsByClassName("my-article");
+article1.style.backgroundColor = 'red';
