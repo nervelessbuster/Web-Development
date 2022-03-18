a1a3f020a4d05de0b8fe685d650ef2e7f1023bd1
nervelessbuster <teslim38@gmail.com>
2022-03-18 22:48:09 +0000

    remove-clutters

--- file.js
+++
@@ -1,5 +0,0 @@
-const header = document.getElementById('myHeader');
-header.style.backgroundColor = 'yellow';
-// const article = document.querySelectorAll('.my-article');
-const article1 = document.getElementsByClassName("my-article");
-article1.style.backgroundColor = 'red';
